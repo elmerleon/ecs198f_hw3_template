57e31701314199b66e1c363515e702912d245fc7
elmerleon <54426335+elmerleon@users.noreply.github.com>
2025-03-01 17:32:47 -0800

    added all tests

--- test_foo_bar_baz.py
+++ test_foo_bar_baz.py
@@ -1,5 +1,31 @@
 import pytest
 
-from foo_bar_baz import foo_bar_baz
+
 
 #Add testcases Here
+def test_modThree():
+    from foo_bar_baz import foo_bar_baz
+    string = foo_bar_baz(3)
+    words = string.split()
+    fooCount = words.count("Foo")
+    assert(fooCount == 1)
+
+def test_madFive():
+    from foo_bar_baz import foo_bar_baz
+    string = foo_bar_baz(5)
+    words = string.split()
+    BarCount = words.count("Bar")
+    assert(BarCount == 1)
+
+def test_modThreeAndFive():
+    from foo_bar_baz import foo_bar_baz
+    string = foo_bar_baz(15)
+    words = string.split()
+    BazCount = words.count("Baz")
+    assert(BazCount == 1)
+
+def test_listEveryNum():
+    from foo_bar_baz import foo_bar_baz
+    string = foo_bar_baz(100)
+    words = string.split()
+    assert(len(words) == 100)
